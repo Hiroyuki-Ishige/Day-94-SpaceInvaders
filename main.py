28a3facdc3e4c8a9c9f775f536207dff9da928ef
ishige <hiroyukiishige@hotmail.com>
2023-06-24 07:48:43 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+import time
+from datetime import datetime
+from turtle import Screen, Turtle
+from tkinter import *
+from tkinter import messagebox
+import pickle
+import pprint
+
+from player_mc import Player
+
+# Fixed variables
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# making screen object
+sc = Screen()
+sc.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+sc.bgcolor("black")
+sc.title("Space invaders")
+
+# create initial screen --------------------------------
+sc.tracer(0)  # Turn OFF screen update. 0 is off, 1 is on
+
+# Create player
+player = Player()
+
+
+sc.tracer(1) # Turn ON screen update. 0 is off, 1 is on
+
+sc.exitonclick()
+# mainloop()
+
+
+
+#TODO create player machine
+#TODO create missile
+#TODO create Invader (& missile from them)
+#TODO create UFO
+#TODO judgement on hit(missile, Invader, UFO)
+#TODO create game over
+#TODO create Score board
+#TODO next stage
+
---
+++ player_mc.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+import time
+
+PLAYER_MOVE_DISTANCE = 40
+PLAYER_WIDTH = 4
+PLAYER_INITIAL_X = 0
+PLAYER_INITIAL_Y = -330
+PLAYER_COLOUR = "white"
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()  # inherit Turtle Class
+        self.shape("square")
+        self.fillcolor(PLAYER_COLOUR)
+        self.shapesize(stretch_wid=1, stretch_len=PLAYER_WIDTH) #Standard turtle size is 20 pixcel x 20 pixcel
+        self.setpos(PLAYER_INITIAL_X, PLAYER_INITIAL_Y)
+
+    def move_left(self):
+        new_x = self.xcor() - PLAYER_MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def move_right(self):
+        new_x = self.xcor() + PLAYER_MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
